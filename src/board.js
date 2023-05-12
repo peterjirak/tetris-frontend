14c6868f4ee8f76ee4c9362bd6055c4e9aacc9d6
Peter Eldritch <peter.jirak@gmail.com>
2023-05-11 21:29:12 -0700

    Implemented getEmptyBoard in src/board.js. Added a call to getEmptyBoard in Board constructor.

--- src/board.js
+++ src/board.js
@@ -1,5 +1,12 @@
-class Board { 
+class Board {
     constructor(ctx) {
-        this.ctx = ctx;    
+        this.ctx = ctx;
+        this.grid = this.getEmptyBoard();
+    }
+   
+    getEmptyBoard() {
+        return Array.from(
+            {length: ROWS}, () => Array(COLS).fill(0)
+        );
     }
 }
