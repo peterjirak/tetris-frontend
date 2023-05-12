b3d120b2f77195ca91b53aadb29a8842be271134
Peter Eldritch <peter.jirak@gmail.com>
2023-05-12 10:49:34 -0700

    Added constants about tetrominos to constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,23 @@
 const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = 30;
+
+const NONE = 'NONE';
+const T = 'T';
+const L = 'L';
+const SQUARE = 'SQUARE';
+const STRAIGHT = 'STRAIGHT';
+const MIRROR_L = 'MIRROR_L';
+const MIRROR_SKEW = 'MIRROR_SKEW';
+
+const TETROMINOS = [ NONE, T, L, SQUARE, STRAIGHT, MIRROR_L, MIRROR_SKEW ];
+
+const TETROMINOS_VALUE_BY_NAME = {
+    NONE: 0,
+    T: 1,
+    L: 2,
+    SQUARE: 3,
+    STRAIGHT: 4,
+    MIRROR_L: 5,
+    MIRROR_SKEW: 6
+};
