a95bad2ca0c27ada96eb1d9f62cd2b89256c4f92
Peter Eldritch <peter.jirak@gmail.com>
2023-05-13 16:57:20 -0700

    Set the GRID_WIDTH to 1.
    Added the SKEW tetromino. I omitted it by mistake.

--- js/constants.js
+++ js/constants.js
@@ -2,26 +2,28 @@
 const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = 30;
-const GRID_WIDTH = 2;
+const GRID_WIDTH = 1;
 
 const NONE = 'NONE';
 const T = 'T';
 const L = 'L';
+const SKEW = 'SKEW';
 const SQUARE = 'SQUARE';
 const STRAIGHT = 'STRAIGHT';
 const MIRROR_L = 'MIRROR_L';
 const MIRROR_SKEW = 'MIRROR_SKEW';
 
-const TETROMINOS = [ NONE, T, L, SQUARE, STRAIGHT, MIRROR_L, MIRROR_SKEW ];
+const TETROMINOS = [ NONE, T, L, SKEW, SQUARE, STRAIGHT, MIRROR_L, MIRROR_SKEW ];
 
 const TETROMINOS_VALUE_BY_NAME = {
     NONE: 0,
     T: 1,
     L: 2,
-    SQUARE: 3,
-    STRAIGHT: 4,
-    MIRROR_L: 5,
-    MIRROR_SKEW: 6
+    SKEW: 3,
+    SQUARE: 4,
+    STRAIGHT: 5,
+    MIRROR_L: 6,
+    MIRROR_SKEW: 7
 };
 
 const TEA_GREEN = 'TEA_GREEN';
