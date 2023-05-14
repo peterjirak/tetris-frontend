ea1cabe277ebfba7eefbedec070d7f43f629fb37
Peter Eldritch <peter.jirak@gmail.com>
2023-05-14 08:21:18 -0700

    Adding color names, color definitions and colors for pieces to the constants.js file. I may eventually move these into a palette.js file instead.

--- js/constants.js
+++ js/constants.js
@@ -3,6 +3,10 @@ const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = 30;
 const GRID_WIDTH = 1;
+const BORDER_WIDTH = 5;
+
+const BORDER = 'BORDER';
+const FILL = 'FILL';
 
 const NONE = 'NONE';
 const T = 'T';
@@ -28,8 +32,72 @@ const TETROMINOS_VALUE_BY_NAME = {
 
 const TEA_GREEN = 'TEA_GREEN';
 const LIGHT_GREEN = 'LIGHT_GREEN';
+const FRENCH_ROSE = 'FRENCH_ROSE';
+const CYCLAMEN = 'CYCLAMEN';
+const AZURE = 'AZURE';
+const JORDY_BLUE = 'JORDY_BLUE';
+const PERSIMMON = 'PERSIMMON';
+const SANDY_BROWN = 'SANDY_BROWN';
+const MAIZE = 'MAIZE';
+const XANTHOUS = 'XANTHOUS';
+const VERONICA = 'VERONICA';
+const INDIGO = 'INDIGO';
+const IMPERIAL_RED = 'IMPERIAL_RED';
+const ENGINEERING_ORANGE = 'ENGINEERING_ORANGE';
+const ICE_BLUE = 'ICE_BLUE';
+const ROBIN_EGG_BLUE = 'ROBIN_EGG_BLUE';
+
+const GRID_COLOR = LIGHT_GREEN;
+const EMPTY_CELL_COLOR = TEA_GREEN;
+
+const TETROMINO_COLORS = {
+    [NONE]: {
+        [BORDER]: EMPTY_CELL_COLOR,
+        [FILL]: EMPTY_CELL_COLOR
+    },
+    [T]: {
+        [BORDER]: INDIGO,
+        [FILL]: VERONICA
+    },
+    [L]: {
+        [BORDER]: PERSIMMON,
+        [FILL]: SANDY_BROWN
+    },
+    [SKEW]: {
+        [BORDER]: ENGINEERING_ORANGE,
+        [FILL]: IMPERIAL_RED
+    },
+    [SQUARE]: {
+        [BORDER]: XANTHOUS,
+        [FILL]: MAIZE
+    },
+    [STRAIGHT]: {
+        [BORDER]: ROBIN_EGG_BLUE,
+        [FILL]: ICE_BLUE
+    },
+    [MIRROR_L]: {
+        [BORDER]: AZURE,
+        [FILL]: JORDY_BLUE
+    },
+    [MIRROR_SKEW]: {
+        [BORDER]: FRENCH_ROSE,
+        [FILL]: CYCLAMEN
+    }
+};
 
 const palette = {
     [TEA_GREEN]: '#D6FFD8',
-    [LIGHT_GREEN]: '#AFFEB1'
+    [LIGHT_GREEN]: '#AFFEB1',
+    [FRENCH_ROSE]: '#F63C80',
+    [CYCLAMEN]: '#FC73A1',
+    [PERSIMMON]: '#E05C15',
+    [SANDY_BROWN]: '#FDA35D',
+    [MAIZE]: '#F6E051',
+    [XANTHOUS]: '#FDB849',
+    [VERONICA]: '#A62BF3',
+    [INDIGO]: '#65099A',
+    [IMPERIAL_RED]: '#F33F45',
+    [ENGINEERING_ORANGE]: '#C00C12',
+    [ICE_BLUE]: '#8DF7F7',
+    [ROBIN_EGG_BLUE]: '#16DFDF'
 };
