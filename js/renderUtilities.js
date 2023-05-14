a22997ebcd02032278c02b4a3d77d16dc78f7e0e
Peter Eldritch <peter.jirak@gmail.com>
2023-05-14 08:17:59 -0700

    Moving some of the render functions out of individual JavaScript classes and into a common file, js/renderUtilities.js . I was using some of the functions in both the Board and Piece classes, so moving them into a common file so they could be used where needed made since. They may eventually end up back in the individual classes once my project is further along.

---
+++ js/renderUtilities.js
@@ -0,0 +1,61 @@
+const renderGridBorder = (ctx, rowIndex, columnIndex) => {
+    const leftX = columnIndex * BLOCK_SIZE;
+    const upperY = rowIndex * BLOCK_SIZE;
+    // TOP
+    ctx.fillStyle = palette[GRID_COLOR];
+    ctx.fillRect(leftX, upperY, BLOCK_SIZE, GRID_WIDTH);
+    // LEFT
+    ctx.fillRect(leftX, upperY + GRID_WIDTH, GRID_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2);
+    // RIGHT
+    ctx.fillRect(leftX + BLOCK_SIZE - GRID_WIDTH, upperY + GRID_WIDTH, GRID_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2);
+    // BOTTOM
+    ctx.fillRect(leftX, upperY + BLOCK_SIZE - GRID_WIDTH, BLOCK_SIZE, GRID_WIDTH);
+    return;
+}
+
+const renderEmptySquare = (ctx, rowIndex, columnIndex) => {
+    renderGridBorder(ctx, rowIndex, columnIndex);
+    const leftX = columnIndex * BLOCK_SIZE + GRID_WIDTH;
+    const upperY = rowIndex * BLOCK_SIZE + GRID_WIDTH;
+    ctx.fillStyle = palette[EMPTY_CELL_COLOR];
+    // FILL
+    ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2, BLOCK_SIZE - GRID_WIDTH * 2);
+    return;
+}
+
+const renderSquareBorder = (ctx, tetrominoType, rowIndex, columnIndex) => {
+    const leftX = columnIndex * BLOCK_SIZE + GRID_WIDTH;
+    const upperY = rowIndex * BLOCK_SIZE + GRID_WIDTH;
+    const borderColorName = TETROMINO_COLORS[tetrominoType].BORDER;
+    const borderColorHex = palette[borderColorName];
+    ctx.fillStyle = borderColorHex;
+    ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2, BLOCK_SIZE - GRID_WIDTH * 2);
+    // TOP
+    ctx.fillStyle = palette[GRID_COLOR];
+    ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2, BORDER_WIDTH);
+    // LEFT
+    ctx.fillRect(leftX, upperY + BORDER_WIDTH, BORDER_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2);
+    // RIGHT
+    ctx.fillRect(leftX + BLOCK_SIZE - GRID_WIDTH - BORDER_WIDTH, upperY + BORDER_WIDTH, BORDER_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2);
+    // BOTTOM
+    ctx.fillRect(leftX, upperY + BLOCK_SIZE - GRID_WIDTH, BLOCK_SIZE, GRID_WIDTH);
+    return;
+}
+
+const renderSquareFill = (ctx, tetrominoType, rowIndex, columnIndex) => {
+    const leftX = columnIndex * BLOCK_SIZE + GRID_WIDTH + BORDER_WIDTH;
+    const upperY = rowIndex * BLOCK_SIZE + GRID_WIDTH + BORDER_WIDTH;
+    const borderColorName = TETROMINO_COLORS[tetrominoType].BORDER;
+    const borderColorHex = palette[borderColorName];
+    ctx.fillStyle = borderColorHex;
+    // FILL
+    ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2, BLOCK_SIZE - GRID_WIDTH * 2  - BORDER_WIDTH * 2);
+    return;
+}
+
+const renderSquare = (ctx, tetrominoType, rowIndex, columnIndex) => {
+    renderGridBorder(ctx, rowIndex, columnIndex);
+    renderSquareBorder(ctx, tetrominoType, rowIndex, columnIndex);
+    renderSquareFill(ctx, tetrominoType, rowIndex, columnIndex);
+    return;
+};
