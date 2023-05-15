4d76879c112e65d2c804d41a4494fb0756140685
Peter Eldritch <peter.jirak@gmail.com>
2023-05-14 18:37:45 -0700

    Fixed defects in renderSquareBorder and renderSquareFill

--- js/renderUtilities.js
+++ js/renderUtilities.js
@@ -29,25 +29,23 @@ const renderSquareBorder = (ctx, tetrominoType, rowIndex, columnIndex) => {
     const borderColorName = TETROMINO_COLORS[tetrominoType].BORDER;
     const borderColorHex = palette[borderColorName];
     ctx.fillStyle = borderColorHex;
-    ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2, BLOCK_SIZE - GRID_WIDTH * 2);
     // TOP
-    ctx.fillStyle = palette[GRID_COLOR];
     ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2, BORDER_WIDTH);
     // LEFT
     ctx.fillRect(leftX, upperY + BORDER_WIDTH, BORDER_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2);
     // RIGHT
-    ctx.fillRect(leftX + BLOCK_SIZE - GRID_WIDTH - BORDER_WIDTH, upperY + BORDER_WIDTH, BORDER_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2);
+    ctx.fillRect(leftX + BLOCK_SIZE - BORDER_WIDTH - GRID_WIDTH * 2, upperY + BORDER_WIDTH, BORDER_WIDTH, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2);
     // BOTTOM
-    ctx.fillRect(leftX, upperY + BLOCK_SIZE - GRID_WIDTH, BLOCK_SIZE, GRID_WIDTH);
+    ctx.fillRect(leftX, upperY + BLOCK_SIZE - BORDER_WIDTH - GRID_WIDTH * 2, BLOCK_SIZE - GRID_WIDTH * 2, BORDER_WIDTH);
     return;
 }
 
 const renderSquareFill = (ctx, tetrominoType, rowIndex, columnIndex) => {
     const leftX = columnIndex * BLOCK_SIZE + GRID_WIDTH + BORDER_WIDTH;
     const upperY = rowIndex * BLOCK_SIZE + GRID_WIDTH + BORDER_WIDTH;
-    const borderColorName = TETROMINO_COLORS[tetrominoType].BORDER;
-    const borderColorHex = palette[borderColorName];
-    ctx.fillStyle = borderColorHex;
+    const fillColorName = TETROMINO_COLORS[tetrominoType].FILL;
+    const fillColorHex = palette[fillColorName];
+    ctx.fillStyle = fillColorHex;
     // FILL
     ctx.fillRect(leftX, upperY, BLOCK_SIZE - GRID_WIDTH * 2 - BORDER_WIDTH * 2, BLOCK_SIZE - GRID_WIDTH * 2  - BORDER_WIDTH * 2);
     return;
